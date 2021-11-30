35e45a450d2acdc1b8a7cf3db2521802e1888d73
Mike Griese <migrie@microsoft.com>
2021-11-30 15:51:48 -0600

    shockingly the component module project seems to compile just fine.

--- ModulesExperiments/CppWinRTModule/pch.h
+++ ModulesExperiments/CppWinRTModule/pch.h
@@ -7,39 +7,39 @@
 
 #pragma once
 
-#include <winrt/Windows.ApplicationModel.h>
-#include <winrt/Windows.ApplicationModel.DataTransfer.h>
-#include <winrt/Windows.Foundation.h>
-#include <winrt/Windows.Foundation.Collections.h>
-#include <winrt/Windows.Foundation.Metadata.h>
-#include <winrt/Windows.Globalization.h>
-#include <winrt/Windows.Graphics.Display.h>
-#include <winrt/Windows.System.h>
-#include <winrt/Windows.UI.Core.h>
-#include <winrt/Windows.UI.Input.h>
-#include <winrt/Windows.UI.Text.h>
-#include <winrt/Windows.UI.ViewManagement.h>
-#include <winrt/Windows.UI.Xaml.Automation.Peers.h>
-#include <winrt/Windows.UI.Xaml.Controls.h>
-#include <winrt/Windows.UI.Xaml.Controls.Primitives.h>
-#include <winrt/Windows.UI.Xaml.Documents.h>
-#include <winrt/Windows.UI.Xaml.Input.h>
-#include <winrt/Windows.UI.Xaml.Markup.h>
-#include <winrt/Windows.UI.Xaml.Media.h>
-#include <winrt/Windows.UI.Xaml.Media.Animation.h>
+// #include <winrt/Windows.ApplicationModel.h>
+// #include <winrt/Windows.ApplicationModel.DataTransfer.h>
+// #include <winrt/Windows.Foundation.h>
+// #include <winrt/Windows.Foundation.Collections.h>
+// #include <winrt/Windows.Foundation.Metadata.h>
+// #include <winrt/Windows.Globalization.h>
+// #include <winrt/Windows.Graphics.Display.h>
+// #include <winrt/Windows.System.h>
+// #include <winrt/Windows.UI.Core.h>
+// #include <winrt/Windows.UI.Input.h>
+// #include <winrt/Windows.UI.Text.h>
+// #include <winrt/Windows.UI.ViewManagement.h>
+// #include <winrt/Windows.UI.Xaml.Automation.Peers.h>
+// #include <winrt/Windows.UI.Xaml.Controls.h>
+// #include <winrt/Windows.UI.Xaml.Controls.Primitives.h>
+// #include <winrt/Windows.UI.Xaml.Documents.h>
+// #include <winrt/Windows.UI.Xaml.Input.h>
+// #include <winrt/Windows.UI.Xaml.Markup.h>
+// #include <winrt/Windows.UI.Xaml.Media.h>
+// #include <winrt/Windows.UI.Xaml.Media.Animation.h>
 
-// #include <winrt/Microsoft.Toolkit.Win32.UI.XamlHost.h>
-// #include <winrt/Microsoft.UI.Xaml.Controls.h>
-// #include <winrt/Microsoft.UI.Xaml.Controls.Primitives.h>
-// #include <winrt/Microsoft.UI.Xaml.XamlTypeInfo.h>
+// // #include <winrt/Microsoft.Toolkit.Win32.UI.XamlHost.h>
+// // #include <winrt/Microsoft.UI.Xaml.Controls.h>
+// // #include <winrt/Microsoft.UI.Xaml.Controls.Primitives.h>
+// // #include <winrt/Microsoft.UI.Xaml.XamlTypeInfo.h>
 
-// #include <winrt/Microsoft.Terminal.Core.h>
-// #include <winrt/Microsoft.Terminal.Control.h>
-// #include <winrt/Microsoft.Terminal.TerminalConnection.h>
-// #include <winrt/Microsoft.Terminal.Settings.Editor.h>
-// #include <winrt/Microsoft.Terminal.Settings.Model.h>
-#include <winrt/Windows.Storage.h>
-#include <winrt/Windows.Storage.Provider.h>
-#include <winrt/Windows.Storage.Pickers.h>
+// // #include <winrt/Microsoft.Terminal.Core.h>
+// // #include <winrt/Microsoft.Terminal.Control.h>
+// // #include <winrt/Microsoft.Terminal.TerminalConnection.h>
+// // #include <winrt/Microsoft.Terminal.Settings.Editor.h>
+// // #include <winrt/Microsoft.Terminal.Settings.Model.h>
+// #include <winrt/Windows.Storage.h>
+// #include <winrt/Windows.Storage.Provider.h>
+// #include <winrt/Windows.Storage.Pickers.h>
 
-// #include <windows.ui.xaml.media.dxinterop.h>
+// // #include <windows.ui.xaml.media.dxinterop.h>
---
+++ ModulesExperiments/WinRTComponentModule/pch.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+// #include <unknwn.h>
+// #include <winrt/Windows.Foundation.h>
+// #include <winrt/Windows.Foundation.Collections.h>
+
+#pragma comment(lib, "oleaut32")
+#pragma comment(lib, "ole32")
+#pragma comment(lib, "advapi32")
