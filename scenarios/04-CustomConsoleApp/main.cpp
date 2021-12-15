c82db3a280cefd5927e71d23637fbacba20903a1
Mike Griese <migrie@microsoft.com>
2021-12-15 16:47:31 -0600

    make main build again

--- scenarios/04-CustomConsoleApp/main.cpp
+++ scenarios/04-CustomConsoleApp/main.cpp
@@ -4,14 +4,23 @@
 #ifdef COMPILE_WITH_MODULES
 import winrt;
 
+// Uncomment this line to try out consuming the SimpleCustomComponent module.
+// Spoilers: it doesn't work yet.
+//
+// #define USE_CUSTOM_MODULE 1
+
+#ifdef USE_CUSTOM_MODULE
 import SimpleCustomComponent;
+#else
+// For now, just include it the old way, so the sln builds.
+#include <winrt/SimpleCustomComponent.h>
+#endif
 
 #else
 
 #include <winrt/SimpleCustomComponent.h>
 
 #endif
-
 
 using namespace winrt;
 using namespace Windows::Foundation;
@@ -21,7 +30,7 @@ int main()
     init_apartment();
     Uri uri(L"http://aka.ms/cppwinrt");
     printf("Hello, %ls!\n", uri.AbsoluteUri().c_str());
-    
+
     winrt::SimpleCustomComponent::Class foo{};
 
     printf("Foo was %d,\n", foo.MyProperty());
